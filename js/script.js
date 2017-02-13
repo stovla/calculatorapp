d8561ed868ce9ea9fe52c1348ab1bcfbf1e28aa9
Vlastimir <vlastimirsd@gmail.com>
2017-02-13 04:11:56 +0000

    built calculator app

--- js/script.js
+++ js/script.js
@@ -1,30 +1,43 @@
-var display = document.getElementById('display'), 
-operator = document.querySelectorAll('operator'),
-keys = document.querySelectorAll('.keys span'),
-equal = document.getElementById('equal');
-
-for (var i = 0; i < keys.length; i++) {
-    keys[i].onclick = function (e) {
-        var inputVal = display.innerHTML;
-        let keyVal = this.innerHTML;
-
-        if(keyVal == 'C') {
-            display.innerHTML = '';
-        }
-        else if (keyVal == '=') {
-            var equation = inputVal;
-
-            if (equation) {
-                display.innerHTML = eval(equation);
-            }
-        }
-        else {
-            if (display.innerHTML.slice(-1) == '.') {
-                display.innerHTML.slice(-1).replace(keyVal);
-            }
-            display.innerHTML += keyVal;
-        }
+$(document).ready(function () {
+    var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        operators = ['/', '*', '-', '+', '.'],
+        brackets = [')','('],
+        result,
+        input = [];
+    function update() {
+        result = input.join("");
+        $('#display').val(result);
     }
-}
+    function getValue(value) {
+        if (numbers.includes(Number(value)) || brackets.includes(value)) {
+            input.push(value);
+        } else if (input.length != 0 && operators.includes(input[input.length - 1]) === true) {
+            input.splice(input.length - 1, 1, value);
+        } else if (input.length === 0){
+            console.log('First Value cant be an operator');
+        } else {
+            input.push(value);
+        }
+        update();
+    }
+    function calculate() {
+        result = input.join("");
+        $('#display').val(eval(result));
+    }
+    $('.main-calc input').click(function (e) {
+        if (this.id === "clearAll") {
+            input = [];
+            update();
+        } else if (this.id === 'clearLast') {
+            input.pop();
+            update();
+        } else if (this.id === 'equal') {
+            calculate();
+            input = [];
+        } else {
+            getValue(this.value);
+        }
+        e.preventDefault();
+    });
+});
 
-console.log();
